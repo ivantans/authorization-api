2060cba2c53cde35d109064d1c089d519adba463
Ivan Tanjaya <ivantanjaya07@gmail.com>
2024-10-29 11:24:04 +0700

    Feat: add user agent middleware

---
+++ src/common/middleware/user-agent/user-agent.middleware.ts
@@ -0,0 +1,20 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { PrismaService } from 'src/database/prisma/prisma.service';
+import { UAParser } from 'ua-parser-js';
+
+@Injectable()
+export class UserAgentMiddleware implements NestMiddleware {
+  constructor(private readonly prisma: PrismaService) { }
+  async use(req: Request, res: Response, next: () => void) {
+    const uaParser = new UAParser()
+    const uaResult = uaParser.setUA(req.headers['user-agent']).getUA();
+    const ua = await this.prisma.userAgent.upsert({
+      where: { ua: uaResult },
+      update: {},
+      create: { ua: uaResult }
+    })
+    req['userAgentId'] = ua.id;
+    next();
+  }
+}
