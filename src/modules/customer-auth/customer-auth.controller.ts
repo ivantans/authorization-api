64cc04f1ad512ce972de10deeedaf52563c54d15
Ivan Tanjaya <ivantanjaya07@gmail.com>
2024-11-01 14:37:21 +0700

    Feat: setup customer module

---
+++ src/modules/customer-auth/customer-auth.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, HttpCode, Post } from '@nestjs/common';
+import { CustomerAuthService } from './customer-auth.service';
+import { ApiResponse } from 'src/common/interface/api-response.interface';
+
+@Controller('customer-auth')
+export class CustomerAuthController {
+  constructor(private readonly customerAuthService: CustomerAuthService) { }
+  @Post("login")
+  @HttpCode(200)
+  async login(): Promise<ApiResponse<>> {
+    try {
+    } catch (e) {
+      throw e
+    }
+  }
+  @Post("refresh-token")
+  @HttpCode(200)
+  async generateRefreshToken(): Promise<ApiResponse<>> {
+    try {
+    }
+    catch (e) {
+      throw e;
+    }
+  }
+
+  @Post("register")
+  @HttpCode(200)
+  async register(): Promise<ApiResponse<>> {
+    try {
+    } catch (e) {
+      throw e;
+    }
+  }
+}
--- src/modules/customer-auth/customer-auth.module.ts
+++ src/modules/customer-auth/customer-auth.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CustomerAuthService } from './customer-auth.service';
+import { CustomerAuthController } from './customer-auth.controller';
 
-@Module({})
+@Module({
+  providers: [CustomerAuthService],
+  controllers: [CustomerAuthController]
+})
 export class CustomerAuthModule {}
